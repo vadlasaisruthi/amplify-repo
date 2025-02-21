882eb44fe368d0d7028fbe38fce1c3cc84236eaf
vadlasaisruthi <vadlasaisruthi2002@gmail.com>
2025-02-21 12:59:31 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+const VARIABLE = import.meta.env.VITE_VARIABLE;
 
 function App() {
   return (
     <>
       <h1>Demo project</h1>
+      <p>I am using env variable here: {VARIABLE}</p>
     </>
   );
 }
